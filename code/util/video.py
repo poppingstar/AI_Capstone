21219028b1e4d7afc58d95e39586b2c94795dd37
popingstar <20191469@student.anu.ac.kr>
2025-05-22 10:32:59 +0900

    베이스 작성 완료

--- code/util/video.py
+++ code/util/video.py
@@ -3,9 +3,8 @@ import numpy as np
 from pathlib import Path
 import concurrent.futures as futures
 from typing import Iterable, Generator
-from copy import deepcopy
 from skimage.metrics import structural_similarity as ssim
-
+from itertools import repeat
 
 def aggregate(func):
     def wrapper(paths:Iterable[Path]) -> Generator[Path]:
@@ -64,38 +63,39 @@ def get_frames_at_interval(video_path:str|Path, interval_sec:int) -> list[np.nda
     total_frame = int(get_total_frames(cap))
     step = int(fps*interval_sec)
 
-    frames = []
-    past_frame_idx = 0
-    for frame_idx in range(1, total_frame, step):
-        target_frame = np.random.randint(past_frame_idx, frame_idx)
-        frame = read_frame_at(cap, target_frame)
-        if frame is None:
-            break
-        frames.append(frame)
-        past_frame_idx = frame_idx
-
-    cap.release()
+    try:
+        frames = []
+        past_frame_idx = 0
+        for frame_idx in range(1, total_frame, step):
+            target_frame = np.random.randint(past_frame_idx, frame_idx)
+            frame = read_frame_at(cap, target_frame)
+            if frame is None:
+                break
+            frames.append(frame)
+            past_frame_idx = frame_idx
+    finally:
+        cap.release()
     return frames
 
 
 def filter_similar_imgs(imgs:list[np.ndarray], threshold:float) -> list:
     non_similar = []
     imgs = imgs.copy()
-    while imgs:
+    rgb_imgs = [cv.cvtColor(img ,cv.COLOR_BGR2RGB) for img in rgb_imgs]
+
+    while rgb_imgs:
         temp = []
+        x = rgb_imgs[0]
         non_similar.append(x)
-        x = cv.cvtColor(x, cv.COLOR_BGR2RGB)
-        x = imgs[0]
         
 
-        for y in imgs[1:]:
-            y = cv.cvtColor(y, cv.COLOR_BGR2RGB)
+        for y in rgb_imgs[1:]:
             similarity = ssim(x, im2=y, data_range=255, channel_axis=-1)
             
             if similarity < threshold:
                 temp.append(y)
         
-        imgs = temp
+        rgb_imgs = temp
 
     non_similar = [cv.cvtColor(img, cv.COLOR_RGB2BGR) for img in non_similar]
     return non_similar
@@ -107,30 +107,33 @@ def save_imgs(imgs:np.ndarray, output_dir:Path) -> None:
         cv.imwrite(output_dir/f'{i}.png', img)
 
 
-def process_videos(video_paths:Iterable[Path], output_dir:Path, interval_sec:int):
+def process_videos(paretnt:Path, children:Iterable[Path], output_dir:Path, interval_sec:int):
     output_dir.mkdir(exist_ok=True, parents=True)
-
-    k = video_paths.keys()
     
-    total_frames = []   #좀 문제 있음
-    for video_path in video_paths.values:
-        frame = get_frames_at_interval(video_path,1000)
+    total_frames = []
+    for child in children:
+        frame = get_frames_at_interval(paretnt/child, interval_sec)
         total_frames.extend(frame)
     
     non_similar_frames = filter_similar_imgs(total_frames, 0.9)
 
-    output_dir_sub = output_dir/k.parent
+    output_dir_sub = output_dir/paretnt.name
     save_imgs(non_similar_frames, output_dir_sub)
 
 
 if __name__ == '__main__':
     def main():
         root_dir = Path(fr"E:\Datasets\딥페이크 변조 영상\1.Train\dffs")
-        output_dir = Path(fr"E:\Datasets\outputs")
+        output_dir = repeat(Path(fr"E:\Datasets\outputs"))
+        interval = repeat(1000)
         leafs = get_leaf_files(root_dir)
         grouped_files = get_parents_by_files(leafs, '.mp4')
 
-        with futures.ProcessPoolExecutor(max_workers=24) as executor:
-            executor.map(process_videos, grouped_files.items())
 
-    # main()
+        parents = [k for k in grouped_files.keys()]
+        children = [v for v in grouped_files.values()]
+        with futures.ProcessPoolExecutor(max_workers=24) as executor:
+            executor.map(process_videos, parents, children, output_dir, interval)
+
+
+    main()
